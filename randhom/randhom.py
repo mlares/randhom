e490e910caed0f9bb02139bb7c412ebd64825959
mlares <marcelo.lares@unc.edu.ar>
2020-03-18 23:25:10 -0300

    parsing encapsulated in randhom.py

--- randhom/randhom.py
+++ randhom/randhom.py
@@ -17,6 +17,53 @@ def check_file(sys_args):
             raise SystemExit(1) 
         
         return filename
+
+
+
+def load_parameters(config):
+    #{{{
+    '''
+    Parse paramenters for the simulation from a .ini file
+    '''
+
+    from collections import namedtuple
+    from astropy import units as u
+    import numpy as np
+
+    units = u.Unit(config['exp']['unit'])
+
+    x = (config['exp']['nran']).split('**')
+    nran = pow(float(x[0]), float(x[1]))
+     
+    smin = float(config['exp']['scale_min'])
+    smax = float(config['exp']['scale_max'])
+    sbin = int(config['exp']['scale_nbins'])
+    nr = np.linspace(start=smin, stop=smax, num=sbin, endpoint=True)
+    rh_type = config['exp']['experiment']
+
+    if rh_type in 'CCVT.ccvt.Ccvt': rh_type='ccvt'
+    if rh_type in 'GLASS.glass.Glass': rh_type='glass'
+    if rh_type in 'ZELREC.ZelRec.zelrec.zel_rec.Zel_Rec.ZEL_REC': \
+                   rh_type='zelrec'
+    if rh_type in 'RANCROSS.RanCross.rancross.Ran_Cross.\
+                   ran_cross': rh_type='rancross'
+    if rh_type in 'RANCROSS.RanSplit.ransplit.Ran_Split.\
+                   ran_split': rh_type='ransplit'
+
+    input_dir =  config['dirs']['dir_data'] + config['dirs']['base_in'] +\
+                 rh_type + '/'    
+    nfiles_max = int(config['exp']['nfiles_max'])
+
+
+    names = 'units nran smin smax sbin nr rh_type input_dir nfiles_max'
+    parset = namedtuple('pars', names)
+
+    res = parset(units, nran, smin, smax, sbin, nr, rh_type, \
+            input_dir, nfiles_max)
+
+    return(res)
+
+    #}}}                
  
                                                                  
 
--- randhom/sd.py
+++ randhom/sd.py
@@ -11,39 +11,19 @@ import randhom as rh
 # Load settings
 
 from configparser import ConfigParser
-#filename = rh.check_file(argv)
-filename = rh.check_file([' ', '../sets/config.ini'])
+filename = rh.check_file(argv)
 config = ConfigParser()
 config.read(filename)
 
 #___________________________________
 # Prepare experiment
 
+
+p = rh.load_parameters(config._sections)
+
 from os import listdir
-
-exec('units=u.' + config['exp']['unit'])
-exec('nran=' + config['exp']['nran'])
- 
-smin = float(config['exp']['scale_min'])
-smax = float(config['exp']['scale_max'])
-sbin = int(config['exp']['scale_nbins'])
-nr = np.linspace(start=smin, stop=smax, num=sbin, endpoint=True)
-rh_type = config['exp']['experiment']
-
-if rh_type in 'CCVT.ccvt.Ccvt': rh_type='ccvt'
-if rh_type in 'GLASS.glass.Glass': rh_type='glass'
-if rh_type in 'ZELREC.ZelRec.zelrec.zel_rec.Zel_Rec.ZEL_REC': \
-               rh_type='zelrec'
-if rh_type in 'RANCROSS.RanCross.rancross.Ran_Cross.\
-               ran_cross': rh_type='rancross'
-if rh_type in 'RANCROSS.RanSplit.ransplit.Ran_Split.\
-               ran_split': rh_type='ransplit'
-
-input_dir =  config['dirs']['dir_data'] + config['dirs']['base_in'] +\
-             rh_type + '/'
-xi_files = listdir(input_dir)
-nfiles_max = int(config['exp']['nfiles_max'])
-rh.test_r_len(input_dir + xi_files[0], nr)
+xi_files = listdir(p.input_dir)
+rh.test_r_len(p.input_dir + xi_files[0], p.nr)
 
 
 #___________________________________
@@ -51,10 +31,10 @@ rh.test_r_len(input_dir + xi_files[0], nr)
 
 xi0_rs, xi2_rs, xi4_rs = [], [], []
 
-for xi_file in xi_files[:nfiles_max]:
+for xi_file in xi_files[:p.nfiles_max]:
 
     cnames=['xi_0','xi_2','xi_4','xi_6']
-    xil_rs = ascii.read(input_dir + xi_file,names=cnames)
+    xil_rs = ascii.read(p.input_dir + xi_file,names=cnames)
 
     xi0_rs.append( xil_rs['xi_0'].data )
     xi2_rs.append( xil_rs['xi_2'].data )
@@ -74,11 +54,11 @@ sd_xi4_rs = np.sqrt(np.var(xi4_rs, axis=0, ddof=1))
 
 cnames = ['xi0', 'xi2','xi4','r']
 
-means = Table([mean_xi0_rs,mean_xi2_rs,mean_xi4_rs,nr], names=cnames)
-fname = '../data/out/xi_mean/std_c_{}.txt'.format(nran)
+means = Table([mean_xi0_rs,mean_xi2_rs,mean_xi4_rs,p.nr], names=cnames)
+fname = '../data/out/xi_mean/std_c_{}.txt'.format(p.nran)
 ascii.write(means, fname, overwrite=True)
  
-sds = Table([sd_xi0_rs,sd_xi2_rs,sd_xi4_rs,nr], names=cnames)
-fname = '../data/out/xi_sd/std_c_{}.txt'.format(nran)
+sds = Table([sd_xi0_rs,sd_xi2_rs,sd_xi4_rs,p.nr], names=cnames)
+fname = '../data/out/xi_sd/std_c_{}.txt'.format(p.nran)
 ascii.write(sds, fname, overwrite=True)
  
